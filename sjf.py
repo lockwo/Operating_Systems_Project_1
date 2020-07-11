5a2900c1a67c14857264d6a2ec2317094eb5ad91
JustinM2250 <justinm2250@gmail.com>
2020-07-11 19:43:41 -0400

    adding processes to ready queue

--- sjf.py
+++ sjf.py
@@ -3,3 +3,17 @@ import rand48
 import process
 
 # SJF
+
+def sjf(time, processes, params):
+    # Make the ready queu
+    readyQueue = dict()
+    
+    # Add all current processes
+    for i in range(len(processes)):
+        current_burst_num = processes[i].current_burst_num
+        readyQueue[processes[i].burst_time[current_burst_num]] = processes[i] # add into readyQueue by burst_time
+        # we don't need to update the current_burst_num yet
+
+    print(readyQueue)
+
+
