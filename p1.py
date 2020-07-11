57371a23b6f3e4808598d55fdaa80bd7ae92b970
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 15:22:19 -0600

    Update p1.py

--- p1.py
+++ p1.py
@@ -11,3 +11,39 @@ t_cs = sys.argv[5]
 tau = sys.argv[6]
 t_slice = sys.argv[7]
 rr_add = sys.argv[8]
+
+# FCFS
+time = 0
+while(1):
+
+
+
+
+    time += 1
+
+# SJF
+time = 0
+while(1):
+
+
+
+
+    time += 1
+
+# SRT
+time = 0
+while(1):
+
+
+
+
+    time += 1
+
+# RR
+time = 0
+while(1):
+
+
+
+
+    time += 1
