09d1af337bf77b1007c9dbe94acf84512c27c201
JustinM2250 <justinm2250@gmail.com>
2020-07-11 18:23:22 -0400

    rand() added

--- rand48.py
+++ rand48.py
@@ -1,5 +1,6 @@
 # From https://stackoverflow.com/questions/7287014/is-there-any-drand48-equivalent-in-python-or-a-wrapper-to-it
 from __future__ import division
+import math
 
 class Rand48(object):
     def __init__(self, seed):
@@ -20,4 +21,33 @@ class Rand48(object):
         if n & (1 << 31):
             n -= 1 << 32
         return n
+    # Justin Mai garbage -Alan Turing
+    def rand(self, Lambda):
+        n = (-1) * math.log(self.drand()) / Lambda
+        return n
 
+    
+'''
+PROGRAM START
+'''
+# seed = 123123
+# processes = []
+# # Set the seed value in srand48()
+# rng = Rand48(seed)
+# rng.srand(test.n)
+
+# for i in range(params.n):
+#     arrival_time = math.floor(rng.drand())
+    # if arrival_time > params.upper_bound:
+    #     continue
+
+# Testing that rand() works
+upper_bound = 3000
+
+rand = Rand48(123123)
+for i in range(10):
+    n = rand.rand(.001)
+    if (n > upper_bound):
+        i -= 1
+        continue
+    print(n)
