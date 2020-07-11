0ad0498b7ed1626b93fb422875bd383cbcb2b43a
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 15:38:56 -0600

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,20 +1,25 @@
 import sys
+from process import Process
 
 class Params(object):
     def __init__(self, n, seed, lam, upper_bound, t_cs, tau, t_slice, rr_add):
-        self.n = n
-        self.seed = seed
-        self.lam = lam
-        self.upper_bound = upper_bound
-        self.t_cs = t_cs
-        self.tau = tau
-        self.t_slice = t_slice
+        self.n = int(n)
+        self.seed = int(seed)
+        self.lam = int(lam)
+        self.upper_bound = int(upper_bound)
+        self.t_cs = int(t_cs)
+        self.tau = int(tau)
+        self.t_slice = int(t_slice)
         self.rr_add = rr_add
 
 if len(sys.argv) == 9:
     params = Params(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
 else:
     params = Params(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], "END")
+
+processes = []
+for i in range(params.n):
+    processes.append(Process(chr(i + 65), params))
 
 # FCFS
 time = 0
