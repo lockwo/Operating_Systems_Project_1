08eeeb68a79f7af094748780fed3892d38d21039
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 16:06:40 -0600

    Update process.py

--- process.py
+++ process.py
@@ -1,14 +1,14 @@
-from math import log
-
+from math import log, trunc
 
 class Process(object):
     def __init__(self, name, params, r):
         self.name = name
         self.arrival_time = self.activation(r.drand(), params.lam, params.upper_bound)
+        self.num_burst = trunc(r.drand())
 
 
     def activation(self, r, lam, ub):
         while(1):
-            test = log(r)/lam
+            test = -log(r)/lam
             if test < ub:
                 return int(test)
