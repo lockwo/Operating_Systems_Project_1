fd69aac330102e0978e7cb3f90a69aa75bdaff61
JustinM2250 <justinm2250@gmail.com>
2020-07-11 17:08:35 -0400

    deleting

--- main.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-seed = float(input()) # add long() if we need more than 7 places of precision...
-lambdaValue = float(input())
-upperbound = int(input())
-contextSwitchTime = float(input())
-alpha = float(input())
-
-
-
