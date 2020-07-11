3c9492ef87ae0c51820792fc53c235585358e4fb
JustinM2250 <justinm2250@gmail.com>
2020-07-11 17:04:51 -0400

    taking inputs

---
+++ main.py
@@ -0,0 +1,9 @@
+n = int(input())
+seed = float(input()) # add long() if we need more than 7 places of precision...
+lambdaValue = float(input())
+upperbound = int(input())
+contextSwitchTime = float(input())
+alpha = float(input())
+
+
+
