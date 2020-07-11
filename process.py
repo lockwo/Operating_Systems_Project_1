ff66f2a34ec34f7d2c618a64f1c666b217414c9e
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 16:02:04 -0600

    Update process.py

--- process.py
+++ process.py
@@ -1,6 +1,14 @@
-from rand48 import Rand48
+from math import log
+
 
 class Process(object):
-    def __init__(self, name, params):
+    def __init__(self, name, params, r):
         self.name = name
-        self.arrival_time = 0
+        self.arrival_time = self.activation(r.drand(), params.lam, params.upper_bound)
+
+
+    def activation(self, r, lam, ub):
+        while(1):
+            test = log(r)/lam
+            if test < ub:
+                return test
