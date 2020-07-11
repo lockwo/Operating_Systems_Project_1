788c412e9149ffdf7c3a6929246b534bbe572546
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 15:34:05 -0600

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,24 +1,22 @@
 import sys
 
-if len(sys.argv) == 9:
-    n = sys.argv[1] 
-    seed = sys.argv[2]
-    lam = sys.argv[3]
-    upper_bound = sys.argv[4]
-    t_cs = sys.argv[5]
-    tau = sys.argv[6]
-    t_slice = sys.argv[7]
-    rr_add = sys.argv[8]
-else:
-    n = sys.argv[1] 
-    seed = sys.argv[2]
-    lam = sys.argv[3]
-    upper_bound = sys.argv[4]
-    t_cs = sys.argv[5]
-    tau = sys.argv[6]
-    t_slice = sys.argv[7]
-    rr_add = "END"
+class Params(object):
+    def __init__(self, n, seed, lam, upper_bound, t_cs, tau, t_slice, rr_add):
+        self.n = n
+        self.seed = seed
+        self.lam = lam
+        self.upper_bound = upper_bound
+        self.t_cs = t_cs
+        self.tau = tau
+        self.t_slice = t_slice
+        self.rr_add = rr_add
 
+if len(sys.argv) == 9:
+    params = Params(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
+else:
+    params = Params(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], "END")
+
+print(params.tau)
 # FCFS
 time = 0
 while(1):
