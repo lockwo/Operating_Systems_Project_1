d4e2445158cc3a8a04ea0f47b0e61691a1c732fd
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 16:41:19 -0600

    Update process.py

--- process.py
+++ process.py
@@ -3,18 +3,19 @@ from math import log, floor, ceil
 class Process(object):
     def __init__(self, name, params, r):
         self.name = name
-        self.arrival_time = self.activation(r.drand(), params.lam, params.upper_bound)
+        self.arrival_time = self.activation(r, params.lam, params.upper_bound)
         self.num_burst = floor(r.drand() * 100 + 1)
-        self.burst_time = ceil(r.drand())
-        self.IO_burst = ceil(r.drand())
+        self.burst_time = ceil(r.drand() * 100)
+        self.IO_burst = ceil(r.drand() * 100)
         self.status = 2 # 0 = running, 1 = ready, 2 = waiting
 
 
     def activation(self, r, lam, ub):
         while(1):
-            test = -log(r)/lam
+            test = -log(r.drand())/lam
             if test < ub:
                 return int(test)
+            
 
     
     def __str__(self):
