e46754697bc4161a5b059b1cd90f6f920722b179
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 17:35:18 -0600

    Update print_sim.py

--- print_sim.py
+++ print_sim.py
@@ -1,9 +1,12 @@
 
+
 def p_sim(time, processes, Q, params, algo):
     if time == 0:
         for i in processes:
-            if algo == "FCFS":
+            if algo == "FCFS" or algo == "RR":
                 print("Process", i.name, "[NEW] (arrival time", i.arrival_time, "ms)", i.num_burst, "CPU Bursts")
+            if algo == "SJF" or algo == "SRT":
+                print("Process", i.name, "[NEW] (arrival time", i.arrival_time, "ms)", i.num_burst, "CPU Bursts (tau ", str(i.tau) + "ms)")
         if len(Q) == 0:
             print("time 0ms: Simulator started for", algo, "[Q <empty>]")
         else:
