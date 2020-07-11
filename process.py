82ddf95fbb691f788a9e9ed735d3dd2d70a7c2f2
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 16:14:35 -0600

    Update process.py

--- process.py
+++ process.py
@@ -1,10 +1,12 @@
-from math import log, trunc
+from math import log, floor, ceil
 
 class Process(object):
     def __init__(self, name, params, r):
         self.name = name
         self.arrival_time = self.activation(r.drand(), params.lam, params.upper_bound)
-        self.num_burst = trunc(r.drand())
+        self.num_burst = floor(r.drand() * 100 + 1)
+        self.burst_time = ceil(r.drand())
+        self.IO_burst = ceil(r.drand())
 
 
     def activation(self, r, lam, ub):
