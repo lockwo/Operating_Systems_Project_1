f37cbac1c12e7ee1de7d8a5206fb26a20f42986d
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 15:11:17 -0600

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) != 9:
+    print("ERROR")
+
+n = sys.argv[1] 
+seed = sys.argv[2]
+lam = sys.argv[3]
+upper_bound = sys.argv[4]
+t_cs = sys.argv[5]
+tau = sys.argv[6]
+t_slice = sys.argv[7]
+rr_add = sys.argv[8]
