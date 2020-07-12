8b76f7c8f476a24523f73f4f438e963c52b5a943
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-07-11 18:36:41 -0600

    Update process.py

--- process.py
+++ process.py
@@ -3,14 +3,14 @@ from math import log, floor, ceil
 class Process(object):
     def __init__(self, name, params, r):
         self.name = name
-        self.arrival_time = self.activation(r, params.lam, params.upper_bound)
+        self.arrival_time = int(self.activation(r, params.lam, params.upper_bound))
         self.num_burst = floor(r.drand() * 100 + 1)
         self.current_burst_num = 0
         self.burst_time = []
         self.IO_burst = []
         for i in range(self.num_burst):
-            self.burst_time.append(ceil(r.drand() * 100))
-            self.IO_burst.append(ceil(r.drand() * 100))
+            self.burst_time.append(ceil(self.activation(r, params.lam, params.upper_bound)))
+            self.IO_burst.append(ceil(self.activation(r, params.lam, params.upper_bound)))
         self.IO_burst.pop()
         self.status = 2 # 0 = running, 1 = ready, 2 = waiting
         self.tau = 1/params.lam
@@ -20,7 +20,7 @@ class Process(object):
         while(1):
             test = -log(r.drand())/lam
             if test < ub:
-                return int(test)
+                return test
             
     def __str__(self):
         ret = self.name + ", arrival time, " + str(self.arrival_time) + ", number of bursts, " + str(self.num_burst) + ", CPU bursts, " + str(self.burst_time) + ", I/O bursts, " + str(self.IO_burst) + ", status, " + str(self.status)
